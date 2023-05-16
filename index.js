8be39c4f63b542db510c98525b93f91f46171e20
areej.fatimah <areej.fatimah@expertflow.com>
2023-05-16 15:59:24 +0500

    add username in query params in socket req

--- index.js
+++ index.js
@@ -298,19 +298,8 @@ proxy.on('proxyRes', function (proxyRes, req, res) {
 app.use('/socket.io', function (req, res) {
     logger.info("Request Url " + req.url + " hostname: " + req.hostname)
     logger.info("Request coming from " + req.connection.remoteAddress)
-    console.log("Request body ===========> " + req.body)
-    if (req.body != null) {
-
-        var str = req.body
-        var str2 = str.toString()
-        if (str2.includes("agent")) {
-            var substr = str2.substring(2).replace(/[\\]/g, "").replace("\"{", "{").replace("}\"", "}")
-            if (substr != null) {
-                //logger.info(JSON.parse(substr))
-                username = JSON.parse(substr).agent.username
-            }
-        }
-    }
+    console.log("requestedUsername ===========> ", req.query.username);
+    username = req.query.username
 
     if (address == undefined) {
         if (username == undefined) {
